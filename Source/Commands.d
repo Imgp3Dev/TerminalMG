8db4a7cb8b80fed89dd04d4f39aed6875f747f40
IMGP3DEV <42507604+Imgp3Dev@users.noreply.github.com>
2018-09-19 06:26:25 -0500

    Create Commands.d

---
+++ Source/Commands.d
@@ -0,0 +1,31 @@
+import std.string
+import std.array
+import std.stdio
+/*Will be updated soon*/
+extern(C++) {
+
+void scroll()
+{
+Up[string] .. auto up;
+foreach(Up; .. -up screen .. %u -> Device)
+{
+println(Device);
+}
+immutable Down[string]
+foreach(Down; is(Down)[-down] .. %s)
+{
+alias(Down; Up; Auto; Clone; Antiban; pfp;
+bgColor);
+writeln(Down);
+}
+Up -> %s -> Down -> %s -> Clone -> %s ->
+Auto -> %s -> Antiban -> %s -> pfp -> %s
+-> bgColor -> %s .. [uint] ..
+Commands(struct; assert(Commands; Device));
+foreach(pfp; %s, Device)
+{
+pfp = import(.png; .jpg);
+pfp -> Device;
+
+continue Device(final; true);
+}
